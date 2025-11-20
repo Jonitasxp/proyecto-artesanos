40f7a92f48950b7fcd158822e6c95c6904d6ed22
Jonitasxp <Jonoziel@gmail.com>
2025-11-20 08:37:11 -0600

    Prueba 1

---
+++ src/ui/components/banner.jsx
@@ -0,0 +1,13 @@
+import "./Banner.css";
+
+export default function Banner() {
+  return (
+    <section className="banner">
+      <div className="banner-content">
+        <h1>Bienvenido a Artesanos</h1>
+        <p>Productos auténticos hechos por manos mexicanas.</p>
+        <button className="btn-banner">Explorar</button>
+      </div>
+    </section>
+  );
+}
