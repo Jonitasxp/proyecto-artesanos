7ff8dbe0febe1f014d5e3af17f102618e8dba12f
Jonitasxp <Jonoziel@gmail.com>
2025-11-20 08:48:18 -0600

    VIEWS

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../ui/components/navbar";
-import Banner from "../ui/components/banner";
 import Footer from "../ui/components/footer";
+import Footer from "../ui/components/banner";
+import Home from "../ui/views/Home";
+import Products from "../ui/views/Products";
+import Contact from "../ui/views/Contact";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Banner />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
